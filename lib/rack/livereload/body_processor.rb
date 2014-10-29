41030cb76520a2c215e753e024d7fa11cced4c96
Stefan Wrobel <swrobel@users.noreply.github.com>
2014-10-28 16:35:21 -1000

    Use https in livereload js tag

--- lib/rack/livereload/body_processor.rb
+++ lib/rack/livereload/body_processor.rb
@@ -10,7 +10,7 @@ module Rack
       attr_reader :content_length, :new_body, :livereload_added
 
       def livereload_local_uri
-        "http://localhost:#{@options[:live_reload_port]}/livereload.js"
+        "https://localhost:#{@options[:live_reload_port]}/livereload.js"
       end
 
       def initialize(body, options)
@@ -109,4 +109,3 @@ module Rack
     end
   end
 end
-
